52a1ec0224e6b987211aa8660f0be08426716b6f
Dorian Bucknor <bucknor.dorian@gmail.com>
2023-10-06 20:22:36 -0500

    added support files for mysql

---
+++ mysql-files/SQL Script.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `Equipment` (
+  `equipmentId` varchar(50),
+  `desctription` text(350),
+  `condition` varchar(50),
+  `name` varchar(80),
+  `category` varchar(50),
+  `rentalStatus` varchar(16),
+  `type` varchar(24),
+  `price` double(16,2),
+  `rentBy` varchar(16),
+  `nextAvailableDate` varchar(50),
+  PRIMARY KEY (`equipmentId`)
+);
+
+CREATE TABLE `Customer` (
+  `customerId` varchar(50),
+  `firstname` varchar(50),
+  `lastName` varchar(50),
+  `address` varchar(80),
+  `phoneNumber` varchar(10),
+  `password` varchar(16),
+  `imageUrl` varchar(150),
+  PRIMARY KEY (`customerId`)
+);
+
+CREATE TABLE `Employee` (
+  `staffId` varchar(50),
+  `firstname` varchar(50),
+  `lastName` varchar(50),
+  `address` varchar(80),
+  `phoneNumber` varchar(10),
+  `imageUrl` varchar(150),
+  `password` varchar(16),
+  PRIMARY KEY (`staffId`)
+);
+
+CREATE TABLE `Invoices` (
+    `invoiceId` INT AUTO_INCREMENT PRIMARY KEY,
+    `customerId` INT,
+    `invoiceDate` DATE,
+    `totalPrice` DECIMAL(10, 2)
+);
+
+CREATE TABLE InvoiceDetails (
+    InvoiceDetailID INT AUTO_INCREMENT PRIMARY KEY,
+    InvoiceID INT,
+    EquipmentID INT,
+    Quantity INT,
+    RentalStartDate DATE,
+    RentalEndDate DATE,
+    
+    FOREIGN KEY (InvoiceID) REFERENCES Invoices(InvoiceID),
+    FOREIGN KEY (EquipmentID) REFERENCES Equipments(EquipmentID)
+);
