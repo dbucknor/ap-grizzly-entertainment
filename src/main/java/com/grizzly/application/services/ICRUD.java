7a8169c474429f3500cbbb21fe454f7ea8f162e4
Dorian Bucknor <bucknor.dorian@gmail.com>
2023-10-26 22:05:34 -0500

    added auth service and CRUD service

---
+++ src/main/java/com/grizzly/application/services/ICRUD.java
@@ -0,0 +1,30 @@
+package com.grizzly.application.services;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.function.Function;
+
+public interface ICRUD<T, K> {
+    final Logger logger = LogManager.getLogger(ICRUD.class);
+
+    public void insert(T record) throws Exception;
+
+    public T read(K id) throws Exception;
+
+
+    public T read(String query, List<?> paramters) throws Exception;
+
+    public List<T> readALL(String query, List<?> paramters) throws Exception;
+
+    public void update(K id) throws Exception;
+
+    public void update(String query, List<?> paramters) throws Exception;
+
+    public void delete(K id) throws Exception;
+
+    public void delete(String query, List<?> paramters) throws Exception;
+
+}
