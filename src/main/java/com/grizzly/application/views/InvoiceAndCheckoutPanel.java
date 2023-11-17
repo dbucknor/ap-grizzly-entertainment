f57a3b487efb1de681787d46b10d9f3798cf7b93
Ricou <ricoyeldemire12@gmail.com>
2023-11-17 11:49:30 -0500

    added the gui for the invoiceandcheckoutpanel and itemrentalgui classes along with the supporting classes for these gui

---
+++ src/main/java/com/grizzly/application/views/InvoiceAndCheckoutPanel.java
@@ -0,0 +1,52 @@
+package com.grizzly.application.views;
+
+import com.grizzly.application.views.components.CheckoutPanel;
+import com.grizzly.application.views.components.InvoicePanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+public class InvoiceAndCheckoutPanel extends JPanel {
+
+    public InvoiceAndCheckoutPanel() {
+        setLayout(new GridBagLayout());
+
+        InvoicePanel invoicePanel = new InvoicePanel();
+        CheckoutPanel checkoutPanel = new CheckoutPanel();
+
+        GridBagConstraints invoiceConstraints = new GridBagConstraints();
+        invoiceConstraints.gridx = 0;
+        invoiceConstraints.gridy = 0;
+        invoiceConstraints.weightx = 1;
+        invoiceConstraints.weighty = 1;
+        invoiceConstraints.fill = GridBagConstraints.BOTH;
+        invoiceConstraints.insets = new Insets(5, 5, 5, 5);
+
+        add(invoicePanel, invoiceConstraints);
+
+        GridBagConstraints checkoutConstraints = new GridBagConstraints();
+        checkoutConstraints.gridx = 1;
+        checkoutConstraints.gridy = 0;
+        checkoutConstraints.weightx = 0;
+        checkoutConstraints.weighty = 0;
+        checkoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+        checkoutConstraints.anchor = GridBagConstraints.NORTH;
+        checkoutConstraints.insets = new Insets(0, 20, 10, 20);
+
+        add(checkoutPanel, checkoutConstraints);
+    }
+
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame();
+//        InvoiceAndCheckoutPanel panel = new InvoiceAndCheckoutPanel();
+//
+//        frame.getContentPane().add(panel);
+//        frame.setSize(new Dimension(800, 600));
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
+//    }
+}
+
---
+++ src/main/java/com/grizzly/application/views/components/CartItem.java
@@ -0,0 +1,168 @@
+package com.grizzly.application.views.components;
+
+import com.grizzly.application.models.InvoiceItem;
+import com.grizzly.application.models.enums.ButtonSize;
+import com.grizzly.application.models.equipment.Equipment;
+import com.grizzly.application.models.interfaces.IView;
+import com.grizzly.application.theme.ThemeManager;
+import com.grizzly.application.views.components.fields.Button;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class CartItem extends JPanel implements IView {
+
+    private JPanel checkPanel;
+    private JLabel name, price, image, periodLbl;
+    private Button button;
+    private ThemeManager theme;
+    private InvoiceItem item;
+
+    public CartItem(InvoiceItem item) {
+        super(new GridBagLayout());
+        this.theme = ThemeManager.getInstance();
+        this.item = item;
+    }
+
+    public void initializeComponents() {
+        periodLbl = new JLabel(item.getRentPeriod().getStart().toString() + "-" + item.getRentPeriod().getEnd().toString());
+        button = new Button("Remove", ButtonSize.SMALL);
+        price = new JLabel(Double.toString(item.getTotalPrice()));
+
+        checkPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        checkPanel.setBackground(theme.getCurrentScheme().getAccent1());
+
+        checkPanel.add(periodLbl);
+        checkPanel.add(price);
+        checkPanel.add(button);
+
+        name = new JLabel(item.getEquipment().getName());
+        image = new JLabel("add image");//todo
+//        image = new JLabel(new ImageIcon(image.getScaledInstance(150, 150, Image.SCALE_SMOOTH));//todo
+    }
+
+    public void addComponents() {
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridheight= 5;
+        c.gridwidth= 5;
+        c.insets = new Insets(0, 0, 0, 5);
+        this.add(image, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridheight = 4;
+        c.gridwidth= 15;
+        this.add(name,c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridheight = 1;
+        c.gridwidth= 15;
+        this.add(periodLbl,c);
+    }
+
+    public void addListeners() {
+        button.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //todo remove from items
+            }
+        });
+    }
+
+    public void setProperties() {
+        Border emptyBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+        Border bottomBorder = BorderFactory.createMatteBorder(0, 0, 4, 0, Color.BLUE);
+        Border compoundBorder = BorderFactory.createCompoundBorder(emptyBorder, bottomBorder);
+
+
+        this.setBackground(theme.getCurrentScheme().getNeutralLight());
+        this.setBorder(compoundBorder);
+        this.setVisible(true);
+    }
+}
+
+/*
+*
+
+    private JPanel listIem(){
+        Border emptyBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
+        Border bottomBorder = BorderFactory.createMatteBorder(0, 0, 4, 0, Color.BLUE);
+        Border compoundBorder = BorderFactory.createCompoundBorder(emptyBorder, bottomBorder);
+
+        JPanel itemPanel = new JPanel(new GridBagLayout());
+                itemPanel.setBackground(new Color(255, 255, 255));
+                itemPanel.setBorder(compoundBorder);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 0, 5);
+
+        Image image = Toolkit.getDefaultToolkit().getImage("");
+        ImageIcon icon = new ImageIcon(image.getScaledInstance(150, 150, Image.SCALE_SMOOTH));
+
+        JTextArea nameTextArea = new JTextArea(ITEM_NAME);
+       nameTextArea.setFont(new Font("Arial", Font.BOLD, 12));
+       nameTextArea
+       nameTextArea.setWrapStyleWord(true);
+       nameTextArea.setPreferredSize(new Dimension(990, 150));
+       itemPanel.add(nameTextArea, c);
+
+
+        JLabel imageLabel = new JLabel(icon);
+        itemPanel.add(imageLabel, c);
+        c.gridx = 1;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 0, 0);
+        // JPanel NamePanel=new JPanel();
+
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 2; // span two columns
+        c.fill = GridBagConstraints.HORIZONTAL; // allow horizontal expansion
+        c.insets = new Insets(5, 0, 0, 0);
+
+        JPanel checkPanel = new JPanel(new FlowLayout());
+        checkPanel.setBackground(color);
+
+        JTextField qtyLabel = new JTextField(ITEM_QTY);
+        qtyLabel.setPreferredSize(new Dimension(190, 30));
+        qtyLabel.setForeground(color5);
+        qtyLabel.setBackground(color);
+        qtyLabel.setBorder(null);
+        qtyLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
+        checkPanel.add(qtyLabel);
+
+        JTextField periodLabel = new JTextField(ITEM_PERIOD);
+        periodLabel.setPreferredSize(new Dimension(200, 30));
+        periodLabel.setForeground(color5);
+        periodLabel.setBackground(color);
+        periodLabel.setBorder(null);
+        periodLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
+        checkPanel.add(periodLabel);
+
+        JTextField priceLabel = new JTextField(ITEM_PRICE);
+        priceLabel.setPreferredSize(new Dimension(200, 30));
+        priceLabel.setForeground(color5);
+        priceLabel.setBackground(color);
+        priceLabel.setBorder(null);
+        priceLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
+        checkPanel.add(priceLabel);
+
+        JButton removeButton = new JButton(REMOVE_BUTTON_TEXT);
+        removeButton.setBackground(color2);
+        removeButton.setForeground(Color.WHITE);
+        checkPanel.add(removeButton);
+
+        itemPanel.add(checkPanel, c);
+        return  panel;
+    }
+
+*
+* */
